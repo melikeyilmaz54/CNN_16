07b0eae2587c64583ff2baf4cecffe90bf0fdc6c
bbmuti <berenmuti5404@gmail.com>
2025-06-12 21:55:13 +0300

    Update CNN_16.v
    assign kısmı ile  cnn_top_module uut  içi düzeltildi

--- CNN_16.v
+++ CNN_16.v
@@ -5,9 +5,10 @@ module CNN_16(
      input sel_in,
      input [15:0] data_in,
      input [11:0] adr_in,
-     output [11:0] tbAR, tbPC,
-     output [15:0] tbIR, tbAC, tbBus, tbDR,
-     output [7:0] tbXREG, tbYREG,
+     output [11:0] PC_Value, AR_Value,
+     output [15:0] IR_Value, AC_Value, TR_Value, DR_Value, bus_value,
+     output [7:0] XREG_Value, YREG_Value,
+     output [15:0] VREG_Value, KREG_Value, GREG_Value, OREG_Value,
      output [7:0] data_out, data_mem_in,
      output we_out
 );
@@ -16,21 +17,29 @@ module CNN_16(
     wire [15:0] from_mem;
     wire [11:0] addr, cpu_adr;
 
-    wire [15:0] testDR, testAC, testIR, testBus;
+    wire [15:0] testIR, testAC, testbus, testDR, testTR;
     wire [11:0] testAR, testPC;
     wire [7:0] testXREG, testYREG;
-    
+    wire [15:0]  testVREG, testKREG, testGREG, testOREG;
+     
     assign data_out=from_mem;
     assign data_mem_in=to_mem;
     assign we_out=we;
     
     //test amaçlý
-    assign tbDR=testDR;
-    assign tbAC=testAC;
-    assign tbPC=testPC;
-    assign tbAR=testAR;
-    assign tbIR=testIR;
-    assign tbBus=testBus;
+    assign AR_Value = testAR;
+    assign address = testAR;
+    assign IR_Value = testIR;
+    assign AC_Value = testAC;
+    assign PC_Value = testPC;
+    assign DR_Value = testDR;
+    assign XREG_Value = testXREG;
+    assign YREG_Value = testYREG;
+    assign VREG_Value = testVREG;
+    assign KREG_Value = testKREG;
+    assign GREG_Value = testGREG;
+    assign OREG_Value = testOREG;
+    assign bus_value =  testbus; 
     
     //dýþardan ram a program yüklemek için
     //sel_in=1 ise RAM a program yükleniyor
@@ -52,9 +61,14 @@ module CNN_16(
         .testAR(testAR), 
         .testPC(testPC),
         .testIR(testIR),
-        .testBus(testBus),
-        .testXREG(),
-        .testYREG()
+        .testTR(testTR), 
+        .testbus(testbus),
+        .testXREG(testXREG),
+        .testYREG(testYREG),
+        .testVREG(testVREG),
+        .testKREG(testKREG),
+        .testGREG(testGREG),
+        .testOREG(testOREG)      
     );
 
     // RAM modülü
